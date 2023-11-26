9b053e6497039a06ab317637f87e8d55059eb22e
Alonso Herreros <alonso.herreros.c@gmail.com>
2023-11-26 20:42:50 +0100

    Solve Problem #6

--- Hilo.java
+++ Hilo.java
@@ -1,26 +1,30 @@
 class Hilo extends Thread {
-    int contador = 0;
+    static int contador = 0; // Changed to `static`
     static int compartida;
 
     public void run() {
-        synchronized (this) {
-            if (contador != 0)
+        synchronized (this.getClass()) { // Changed from `this` to `this.getClass()`
+            while (contador != 0) // Changed from `if` to `while`
                 try {
-                    wait();
+                    this.getClass().wait();
                 } catch (Exception e) {
                 }
             contador++;
         }
         compartida++;
-        notifyAll();
+        synchronized (this.getClass()) {
+            contador--; // Added this decrement to allow other threads to use the resource
+            this.getClass().notifyAll(); // Changed to class method (not instance). Moved inside the `synchronized` block
+        }
     }
 
     public static void main(String[] s) {
         Hilo h1 = new Hilo();
         Hilo h2 = new Hilo();
         Hilo h3 = new Hilo();
-        h1.run();
-        h2.run();
-        h3.run();
+        // These calls were changed from `run()` to `start()`
+        h1.start();
+        h2.start();
+        h3.start();
     }
 }
